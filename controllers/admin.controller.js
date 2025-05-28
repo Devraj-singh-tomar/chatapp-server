fe00439eeb9db15ea3bda54237aaee7a45d3fc9c
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-05-28 18:45:49 +0530

    feat: implement admin login and logout functionality with validation

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -2,7 +2,45 @@ import { TryCatch } from "../middlewares/error.js";
 import { User } from "../models/user.model.js";
 import { Chat } from "../models/chat.model.js";
 import { Message } from "../models/message.model.js";
-// import { ErrorHandler } from "../utils/utility.js";
+import { ErrorHandler } from "../utils/utility.js";
+import jwt from "jsonwebtoken";
+import { cookieOptions } from "../utils/features.js";
+
+export const adminLogin = TryCatch(async (req, res, next) => {
+  const { secretKey } = req.body;
+
+  const adminSecretKey = process.env.ADMIN_SECRET_KEY || "asdasasgdasdasd";
+
+  const isMatched = secretKey === adminSecretKey;
+
+  if (!isMatched) return next(new ErrorHandler("Invalid admin key", 401));
+
+  const token = jwt.sign(secretKey, process.env.JWT_SECRET);
+
+  return res
+    .status(200)
+    .cookie("admin-token", token, {
+      ...cookieOptions,
+      maxAge: 1000 * 60 * 15,
+    })
+    .json({
+      success: true,
+      message: "Admin login successfully",
+    });
+});
+
+export const adminLogout = TryCatch(async (req, res, next) => {
+  return res
+    .status(200)
+    .cookie("admin-token", "", {
+      ...cookieOptions,
+      maxAge: 0,
+    })
+    .json({
+      success: true,
+      message: "Logged out successfully",
+    });
+});
 
 export const getAllUsers = TryCatch(async (req, res, next) => {
   const users = await User.find({});
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,18 +1,22 @@
 import express from "express";
 import {
+  adminLogin,
+  adminLogout,
   getAllChats,
   getAllMessages,
   getAllUsers,
   getDashboardStats,
 } from "../controllers/admin.controller.js";
 
+import { adminLoginValidator, validateHandler } from "../lib/validators.js";
+
 const app = express.Router();
 
 // app.get("/");
 
-// app.post("/verify");
+app.post("/verify", adminLoginValidator(), validateHandler, adminLogin);
 
-// app.get("/logout");
+app.get("/logout", adminLogout);
 
 app.get("/users", getAllUsers);
 
